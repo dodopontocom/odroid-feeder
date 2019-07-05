4c2b0b53b441f13a4d28ec7594c4b355ca052f5b
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-05 16:21:50 -0300

    Update servo.sh

--- bot/functions/servo.sh
+++ bot/functions/servo.sh
@@ -12,16 +12,18 @@ servo.food () {
 					--text "Alimentando seu pet..."
 	
 	if [[ $btn_type == "btn_feed1" ]]; then
+		echo feed1
 		#250g
-		servo1.sh
-		sleep 3
-		servo1.sh
-		selfie.shot
+		#servo1.sh
+		#sleep 3
+		#servo1.sh
+		#selfie.shot
 	elif [[ $btn_type == "btn_feed2" ]]; then
+		echo feed2
 		#150g
-		servo1.sh
-		sleep 3
-		selfie.shot
+		#servo1.sh
+		#sleep 3
+		#selfie.shot
 	fi
 }
 
@@ -33,15 +35,17 @@ servo.water () {
 	ShellBot.answerCallbackQuery --callback_query_id ${callback_query_id[$id]} \
 					--text "Alimentando seu pet..."
 	if [[ $btn_type == "btn_water1" ]]; then
+		echo water1
 		#500ml
-		servo2.sh
-		sleep 3
-		servo2.sh
-		selfie.shot
+		#servo2.sh
+		#sleep 3
+		#servo2.sh
+		#selfie.shot
 	elif [[ $btn_type == "btn_water2" ]]; then
+		echo water2
 		#250ML
-		servo2.sh
-		sleep 3
-		selfie.shot
+		#servo2.sh
+		#sleep 3
+		#selfie.shot
 	fi
 }
