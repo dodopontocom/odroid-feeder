c010adff04cb10101020baad2d9f04872c60559f
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-05 10:18:47 -0300

    Update start.sh

--- bot/functions/start.sh
+++ bot/functions/start.sh
@@ -2,6 +2,8 @@
 #
 BASEDIR=$(dirname $0)
 
+pets_name=$(tail -1 ${BASEDIR}/.pets_name)
+
 start.sendGreetings() {
   local message txt name
   name=$1
@@ -11,10 +13,14 @@ start.sendGreetings() {
     message="*Opção Ajuda*"
     ShellBot.sendMessage --chat_id ${callback_query_message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
     ShellBot.sendMessage --chat_id ${callback_query_message_chat_id[$id]} --text "$(echo -e $(cat ${txt}))" --parse_mode markdown
+    message="Comece pressionando aqui /${pets_name}"
+    ShellBot.sendMessage --chat_id ${callback_query_message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown    
   else
     message="*Olá ${name}*"
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e $(cat ${txt}))" --parse_mode markdown
+    message="Comece pressionando aqui /${pets_name}"
+    ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
   fi
   
   
