a3182c89f2b0d8a1b4faa34a8e8a3868d5900013
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-05 15:16:07 -0300

    Update start.sh

--- bot/functions/start.sh
+++ bot/functions/start.sh
@@ -6,15 +6,18 @@ start.sendGreetings() {
   local message txt name user_id user_log pets_info pets_name
   name=$1
   txt=${BASEDIR}/texts/start.txt
-  
 
-  user_id=${message_chat_id[$id]}  
-  user_log=${BASEDIR}/logs/${message_chat_id[$id]}_${message_from_first_name}
-  pets_info=${user_log}/pets_info.txt
-  
-  pets_name=$(cat $pets_info | grep ^nome | tail -1 | cut -d':' -f2 | cut -d' ' -f1 | tr '[:upper:]' '[:lower:]')
-  
-  if [[ -z $name ]]; then 
+  if [[ -z $name ]]; then
+    user_id=${callback_query_message_chat_id[$id]}  
+    echo $user_id
+    user_log=${BASEDIR}/logs/${user_id}_${callback_query_from_first_name}
+    echo $user_log
+    pets_info=${user_log}/pets_info.txt
+    echo $pets_info
+
+    pets_name=$(cat $pets_info | grep ^nome | tail -1 | cut -d':' -f2 | cut -d' ' -f1 | tr '[:upper:]' '[:lower:]')
+    echo $pets_name
+    
     message="*Opção Ajuda*"
     ShellBot.sendMessage --chat_id ${callback_query_message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
     ShellBot.sendMessage --chat_id ${callback_query_message_chat_id[$id]} --text "$(echo -e $(cat ${txt}))" --parse_mode markdown
@@ -22,6 +25,16 @@ start.sendGreetings() {
     sleep 5
     ShellBot.sendMessage --chat_id ${callback_query_message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown    
   else
+    user_id=${message_chat_id[$id]}  
+    echo $user_id
+    user_log=${BASEDIR}/logs/${user_id}_${message_from_first_name}
+    echo $user_log
+    pets_info=${user_log}/pets_info.txt
+    echo $pets_info
+
+    pets_name=$(cat $pets_info | grep ^nome | tail -1 | cut -d':' -f2 | cut -d' ' -f1 | tr '[:upper:]' '[:lower:]')
+    echo $pets_name
+    
     message="*Olá ${name}*"
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
     ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e $(cat ${txt}))" --parse_mode markdown
