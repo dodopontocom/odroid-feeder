7ec7ed2e69c21a29f999ee7728960f4d9e009c56
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-05 15:06:11 -0300

    Update start.sh

--- bot/functions/start.sh
+++ bot/functions/start.sh
@@ -2,18 +2,17 @@
 #
 BASEDIR=$(dirname $0)
 
-user_id=${message_chat_id[$id]}  
-user_log=${BASEDIR}/logs/${message_chat_id[$id]}_${message_from_first_name}
-pets_info=${user_log}/pets_info.txt
-
-#pets_name=$(tail -1 ${BASEDIR}/.pets_name | cut -d':' -f1)
-pets_name=$(cat $pets_info | grep ^nome | tail -1 | cut -d':' -f2 | cut -d' ' -f1 | tr '[:upper:]' '[:lower:]')
-
-
 start.sendGreetings() {
-  local message txt name
+  local message txt name user_id user_log pets_info pets_name
   name=$1
   txt=${BASEDIR}/texts/start.txt
+  
+
+  user_id=${message_chat_id[$id]}  
+  user_log=${BASEDIR}/logs/${message_chat_id[$id]}_${message_from_first_name}
+  pets_info=${user_log}/pets_info.txt
+  
+  pets_name=$(cat $pets_info | grep ^nome | tail -1 | cut -d':' -f2 | cut -d' ' -f1 | tr '[:upper:]' '[:lower:]')
   
   if [[ -z $name ]]; then 
     message="*Opção Ajuda*"
