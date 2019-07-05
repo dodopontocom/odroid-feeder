3858f585bb7b1b8ea02d8eabc088ba68e1cb020d
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-05 09:59:05 -0300

    Update start.sh

--- bot/functions/start.sh
+++ bot/functions/start.sh
@@ -6,10 +6,10 @@ start.sendGreetings() {
   local message txt
   txt=${BASEDIR}/texts/start.txt
   message="*olá *"
-  if [[ ! -z $message_from_first_name ]]; then
+  if [[ ! -z $callback_query_from_first_name ]]; then
     message+=${message_from_first_name}
   else
-    message+=${message_from_id}
+    message+=${callback_query_from_id}
   fi
   ShellBot.sendMessage --chat_id ${callback_query_message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
   ShellBot.sendMessage --chat_id ${callback_query_message_chat_id[$id]} --text "$(echo -e $(cat ${txt}))" --parse_mode markdown
