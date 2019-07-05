c56cae219077568d95bc3a1ee5e7088ed6b6e87a
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-05 11:57:18 -0300

    Update pet.sh

--- bot/functions/pet.sh
+++ bot/functions/pet.sh
@@ -6,7 +6,7 @@ pet.register() {
   
   sucess_msg="$(date +%H:%M:%S) - pet cadastrado com sucesso"
   user_id=${message_chat_id[$id]}  
-  user_log=${BASEDIR}/${logs}/${message_chat_id[$id]}_${message_from_first_name}
+  user_log=${BASEDIR}/logs/${message_chat_id[$id]}_${message_from_first_name}
   pets_info=${user_log}/pets_info.txt
   
   if [[ ! -d $user_log ]]; then					
@@ -21,3 +21,29 @@ pet.register() {
 
   echo $sucess_msg >> $user_log/$(date +%Y%m%d).log
 }
+pet.nome() {
+  local nome sucess_msg
+  nome=$1
+  sucess_msg="$(date +%H:%M:%S) - $nome cadastrado"
+  
+  user_id=${message_chat_id[$id]}  
+  user_log=${BASEDIR}/logs/${message_chat_id[$id]}_${message_from_first_name}
+  pets_info=${user_log}/pets_info.txt
+  
+  echo "$sucess_msg" >> $user_log/$(date +%Y%m%d).log
+  echo "nome:$nome" >> $pets_info
+}
+
+pet.animal() {
+  local nome sucess_msg
+  animal=$1
+  sucess_msg="$(date +%H:%M:%S) - $animal cadastrado"
+  
+  user_id=${message_chat_id[$id]}  
+  user_log=${BASEDIR}/logs/${message_chat_id[$id]}_${message_from_first_name}
+  pets_info=${user_log}/pets_info.txt
+  
+  echo "$sucess_msg" >> $user_log/$(date +%Y%m%d).log
+  echo "animal:$animal" >> $pets_info
+}
+
