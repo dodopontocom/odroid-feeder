c7593a84d239554be9f12856c7cd8404729a9aa7
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-05 16:05:04 -0300

    Update servo.sh

--- bot/functions/servo.sh
+++ bot/functions/servo.sh
@@ -4,17 +4,44 @@ BASEDIR=$(dirname $0)
 source ${BASEDIR}/selfie.sh
 
 servo.food () {
+	local btn_type
+	
+	btn_type=$callback_query_data
+	
 	ShellBot.answerCallbackQuery --callback_query_id ${callback_query_id[$id]} \
 					--text "Alimentando seu pet..."
-	servo1.sh
-	sleep 3
-	selfie.shot
+	
+	if [[ $btn_type == "btn_feed1" ]]; then
+		#250g
+		servo1.sh
+		sleep 3
+		servo1.sh
+		selfie.shot
+	elif [[ $btn_type == "btn_feed2" ]]; then
+		#150g
+		servo1.sh
+		sleep 3
+		selfie.shot
+	fi
 }
 
 servo.water () {
+	local btn_type
+	
+	btn_type=$callback_query_data
+	
 	ShellBot.answerCallbackQuery --callback_query_id ${callback_query_id[$id]} \
 					--text "Alimentando seu pet..."
-	servo2.sh
-	sleep 3
-	selfie.shot
+	if [[ $btn_type == "btn_water1" ]]; then
+		#500ml
+		servo2.sh
+		sleep 3
+		servo2.sh
+		selfie.shot
+	elif [[ $btn_type == "btn_water2" ]]; then
+		#250ML
+		servo2.sh
+		sleep 3
+		selfie.shot
+	fi
 }
