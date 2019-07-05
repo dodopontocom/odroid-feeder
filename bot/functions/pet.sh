103d158dcfeae86c7e1caed5cf9f546f5f7d9156
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-05 12:53:45 -0300

    Update pet.sh

--- bot/functions/pet.sh
+++ bot/functions/pet.sh
@@ -65,8 +65,7 @@ pet.animal() {
   message="Cadastro realizado com sucesso"
   ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})"
   
-  message="O comando para me chamar é \/$(cat $pets_info | grep ^nome | cut -d':' -f2 | cut -d' ' -f1 | tr '[:upper:]' '[:lo
-wer:]')"
+  message="O comando para me chamar é \/$(cat $pets_info | grep ^nome | cut -d':' -f2 | cut -d' ' -f1 | tr '[:upper:]' '[:lower:]')"
   ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})"
   
 }
