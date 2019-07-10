c1cb3278fa4133ccceb415cb324392285ed5e88b
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-10 10:43:26 -0300

    Update remove_acentos.sh

--- bot/functions/remove_acentos.sh
+++ bot/functions/remove_acentos.sh
@@ -4,6 +4,6 @@ BASEDIR=$(dirname $0)
 remove.acento() {
   local str ret_str
   str=$1
-  ret_str="$(echo "$str" | sed 'y/áÁàÀãÃâÂéÉêÊíÍóÓõÕôÔúÚçÇ/aAaAaAaAeEeEiIoOoOoOuUcC/')"
+  ret_str="$(echo "$str" | sed 's/áÁàÀãÃâÂéÉêÊíÍóÓõÕôÔúÚçÇ/aAaAaAaAeEeEiIoOoOoOuUcC/g')"
   echo $ret_str
 }
