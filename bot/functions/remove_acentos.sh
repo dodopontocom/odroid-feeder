301b3cdbf6a3e47ad888ca7c74b638429c79ef47
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-10 09:21:34 -0300

    Create remove_acentos.sh

---
+++ bot/functions/remove_acentos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+BASEDIR=$(dirname $0)
+
+remove.acento() {
+  local str ret_str
+  str=$1
+  ret_str="$(echo "$str" | sed 'y/áÁàÀãÃâÂéÉêÊíÍóÓõÕôÔúÚçÇ/aAaAaAaAeEeEiIoOoOoOuUcC/')"
+  echo $ret_str
+}
