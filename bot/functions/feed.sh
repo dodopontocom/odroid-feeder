ff9cde8f991b375ddb891df464ebb084810c7cbb
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-11 15:06:13 -0300

    Update feed.sh

--- bot/functions/feed.sh
+++ bot/functions/feed.sh
@@ -15,6 +15,7 @@ feed.init() {
 
   log_message="$(date +%H:%M:%S) - comando /${pets_name} executado"
   echo $log_message >> $user_log/$(date +%Y%m%d).log
+  echo $log_message # log into main log
   
   message="*${pets_action} ... Estou com fome ${pets_dono} ...*"
   ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" \
