87abf302a9d2f549125d8d84ab4226c50ee48b18
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-12 12:44:59 -0300

    Update bot.sh

--- bot/bot.sh
+++ bot/bot.sh
@@ -15,7 +15,7 @@ source ${BASEDIR}/functions/servo.sh
 source ${BASEDIR}/functions/user.sh
 source ${BASEDIR}/functions/support.sh
 source ${BASEDIR}/functions/cron.sh
-source ${BASEDIR}/functions/bot-teclados.sh
+source ${BASEDIR}/bot-teclados.sh
 
 id_check=${BASEDIR}/.id_registrados
 admins_id=${BASEDIR}/.admins_id
